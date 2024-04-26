2eaa3ac3996b9d662dd78cac1571994289839e77
Ankit <ankithooda23@gmail.com>
2024-04-26 21:32:33 +0530

    Added testing boilerplate to chapter_2

--- chapter_2/00_intro.cpp
+++ chapter_2/00_intro.cpp
@@ -1,42 +1,50 @@
 #include <iostream>
 #include <limits>
+#include <random>
 
 void step(float* r, const float* d, int n);
 
 int main() {
-    constexpr int n = 3;
-    const float d[n*n] = {
-        0, 8, 2,
-        1, 0, 9,
-        4, 5, 0,
-    };
-    float r[n*n];
+    constexpr int n = 3000;
+    float *d = (float*)malloc(sizeof(float) * n * n);
+
+    // Fill with random numbers
+    std::random_device seed;
+    std::mt19937 gen{seed()}; // seed the generator
+    std::uniform_int_distribution<> dist{1, 10000}; // set min and max
+    for (int j = 0; j < n; j++) {
+      for (int i = 0; i < n; i++) {
+        d[j*n+i] = (float)dist(gen);
+      }
+    }
+
+    float *r = (float*)malloc(sizeof(float) * n * n);
+    std::cout << "Start\n";
     step(r, d, n);
+    /*
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             std::cout << r[i*n + j] << " ";
         }
         std::cout << "\n";
     }
+    */
+    std::cout << "Done\n";
+
 }
 
 void step(float *r, const float* d, int n) {
-  float t;
-
   for (unsigned int j = 0; j < n; j++) {
     for (unsigned int i = 0; i < n; i++) {
-      r[j*n+i] = std::numeric_limits<float>::infinity();;
-    }
-  }
+      float v = std::numeric_limits<float>::infinity();
 
-  for (unsigned int j = 0; j < n; j++) {
-    for (unsigned int i = 0; i < n; i++) {
       for (unsigned int k = 0; k < n; k++) {
-        t = d[k*n+i] + d[j*n+k];
-        if (t < r[j*n+i]) {
-          r[j*n+i] = t;
-        }
+        float x = d[k*n+i];
+        float y = d[j*n+k];
+        float z = x + y;
+        v = std::min(v, z);
       }
+      r[j*n+i] = v;
     }
   }
 }
