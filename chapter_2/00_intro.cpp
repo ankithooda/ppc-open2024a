5dd80fa23eac73ca73fb5b0c38aa82495b649c43
Ankit <ankithooda23@gmail.com>
2024-05-04 00:37:16 +0530

    Chapter 2 : Use transpose for linear reading

--- chapter_2/00_intro.cpp
+++ chapter_2/00_intro.cpp
@@ -4,40 +4,68 @@
 #include <x86intrin.h>
 
 int main() {
-  constexpr int n = 1600;
+  constexpr int n = 3;
   float *d = (float*)malloc(sizeof(float) * n * n);
 
-  /*
-    const float d[n*n] = {
-      0, 8, 2,
-      1, 0, 9,
-      4, 5, 0,
-      };*/
+  d[0] = 0;
+  d[1] = 8;
+  d[2] = 2;
+
+  d[3] = 1;
+  d[4] = 0;
+  d[5] = 9;
+
+  d[6] = 4;
+  d[7] = 5;
+  d[8] = 0;
 
     // Fill with random numbers
-    std::random_device seed;
-    std::mt19937 gen{seed()}; // seed the generator
-    std::uniform_int_distribution<> dist{1, 10000}; // set min and max
-    for (int j = 0; j < n; j++) {
-      for (int i = 0; i < n; i++) {
-        d[j*n+i] = (float)dist(gen);
-      }
-    }
+    // std::random_device seed;
+    // std::mt19937 gen{seed()}; // seed the generator
+    // std::uniform_int_distribution<> dist{1, 10000}; // set min and max
+    // for (int j = 0; j < n; j++) {
+    //   for (int i = 0; i < n; i++) {
+    //     d[j*n+i] = (float)dist(gen);
+    //   }
+    // }
     unsigned long after, before;
 
 
 
     float *r = (float*)malloc(sizeof(float) * n * n);
+    float *t = (float*)malloc(sizeof(float) * n * n);
+
     std::cout << "Start\n";
 
     before = __rdtsc();
+
+    for (unsigned int j = 0; j < n; j++) {
+      for (unsigned int i = 0; i < n; i++) {
+        t[i * n + j] = d[j * n + i];
+      }
+    }
+
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        std::cout << d[i*n + j] << " ";
+      }
+      std::cout << "\n";
+    }
+    std::cout << "\n#################\n";
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        std::cout << t[i*n + j] << " ";
+      }
+      std::cout << "\n";
+    }
+
     for (unsigned int j = 0; j < n; j++) {
       for (unsigned int i = 0; i < n; i++) {
         float v = std::numeric_limits<float>::infinity();
 
         for (unsigned int k = 0; k < n; k++) {
-          float x = d[k*n+i];
-          float y = d[j*n+k];
+          float x = d[j*n+k];
+          float y = t[i*n+k];
           float z = x + y;
           v = std::min(v, z);
         }
@@ -46,11 +74,11 @@ int main() {
     }
     after = __rdtsc();
     std::cout << after - before << " " << "Done\n";
-    // for (int i = 0; i < n; ++i) {
-    //     for (int j = 0; j < n; ++j) {
-    //         std::cout << r[i*n + j] << " ";
-    //     }
-    //     std::cout << "\n";
-    // }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            std::cout << r[i*n + j] << " ";
+        }
+        std::cout << "\n";
+    }
 }
 
