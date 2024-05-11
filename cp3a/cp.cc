be306f275eebb55b1e0fd2c49b1280f56122d042
Ankit <ankithooda23@gmail.com>
2024-05-12 01:57:18 +0530

    CP3A : Commented out rdtsc code

--- cp3a/cp.cc
+++ cp3a/cp.cc
@@ -72,7 +72,7 @@ void correlate(int orig_y, int orig_x, const float *data, float *result) {
   double *row_means = (double *)malloc(sizeof(double) * ny);
   double *DT = (double *)malloc(sizeof(double) * nx * ny);
 
-  before = __rdtsc();
+  //before = __rdtsc();
   // COPY ORIGINAL DATA TO A DOUBLE FLOAT MATRIX
   // WHICH WILL ACT AS SOURCE FOR CREATING __m256d MATRIX.
   // This needs to be done because __m256d gets loaded with
@@ -84,8 +84,8 @@ void correlate(int orig_y, int orig_x, const float *data, float *result) {
       DT[c + r * nx] = (double)data[c + r * nx];
     }
   }
-  after = __rdtsc();
-  std::cout << after - before << " DT Copy \n";
+  //after = __rdtsc();
+  //std::cout << after - before << " DT Copy \n";
 
   // Vector Matrix
   __mmask8 pad_mask;
@@ -104,7 +104,7 @@ void correlate(int orig_y, int orig_x, const float *data, float *result) {
   // Run the loop for pad_nx - 1
   // because pad_nx - 1 vectors will alway be completely filled.
   // Only the last vector can be partially filled.
-  before = __rdtsc();
+  //before = __rdtsc();
   #pragma omp parallel for
   for (unsigned int r = 0; r < ny; r++) {
     pad_mask = _cvtu32_mask8(15);
@@ -117,8 +117,8 @@ void correlate(int orig_y, int orig_x, const float *data, float *result) {
     //std::cout << mask_bits << " " << nx - ((pad_nx - 1) * capacity) << " " << nx << " " << pad_nx << "\n";
     IT[pad_nx - 1 + r * pad_nx] = _mm256_maskz_expandloadu_pd(pad_mask, DT + ((pad_nx - 1) * capacity) + r * nx);
   }
-  after = __rdtsc();
-  std::cout << after - before << " Vector Copy \n";
+  //after = __rdtsc();
+  //std::cout << after - before << " Vector Copy \n";
 
   //std::cout << "Printing original data \n";
   //print_m(ny, nx, data);
@@ -128,7 +128,7 @@ void correlate(int orig_y, int orig_x, const float *data, float *result) {
 
   // Vector operations.
   // Calculate Mean for each row using Vector operations.
-  before = __rdtsc();
+  //before = __rdtsc();
   #pragma omp parallel for
   for (unsigned int r = 0; r < ny; r++) {
     double sum = 0;
@@ -168,8 +168,8 @@ void correlate(int orig_y, int orig_x, const float *data, float *result) {
     pad_mask = _knot_mask8(pad_mask);
     IT[pad_nx - 1 + r * pad_nx] = _mm256_mask_and_pd(IT[pad_nx - 1 + r * pad_nx], pad_mask, IT[pad_nx - 1 + r * pad_nx], zeros);
   }
-  after = __rdtsc();
-  std::cout << after - before << " 1st norm \n";
+  //after = __rdtsc();
+  //std::cout << after - before << " 1st norm \n";
 
   // std::cout << "Priniting vector matrix after mean normalization\n";
   // print_matrix_vector_double(ny, pad_nx, IT);
@@ -177,7 +177,7 @@ void correlate(int orig_y, int orig_x, const float *data, float *result) {
 
   // VECTOR OPS
   // Calculate Squared Sum of this new matrix.
-  before = __rdtsc();
+  //before = __rdtsc();
   #pragma omp parallel for
   for (unsigned int r = 0; r < ny; r++) {
     double sq_sum = 0;
@@ -208,8 +208,8 @@ void correlate(int orig_y, int orig_x, const float *data, float *result) {
     // We do not need to zero the padded doubles in this normalization.
     // because they were already zero and getting divided by root will also produce zero.
   }
-  after = __rdtsc();
-  std::cout << after - before << " 2nd norm \n";
+  //after = __rdtsc();
+  //std::cout << after - before << " 2nd norm \n";
 
 
   // std::cout << "Priniting vector matrix after sq sum normalization\n";
@@ -218,7 +218,7 @@ void correlate(int orig_y, int orig_x, const float *data, float *result) {
 
   // VECTOR IMPLEMENTATION
 
-  before = __rdtsc();
+  //before = __rdtsc();
   #pragma omp parallel for
   for (unsigned int r = 0; r < ny; r++) {
     //#pragma omp parallel for
@@ -244,8 +244,8 @@ void correlate(int orig_y, int orig_x, const float *data, float *result) {
       }
     }
   }
-  after = __rdtsc();
-  std::cout << after - before << " Matrix calc \n";
+  //after = __rdtsc();
+  //std::cout << after - before << " Matrix calc \n";
 
   // Free all allocated memory
   free(IT);
