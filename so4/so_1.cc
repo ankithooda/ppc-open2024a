a939245e9821e911e5c4f408ab95e085968f5828
Ankit <ankithooda23@gmail.com>
2024-05-22 22:39:41 +0530

    SO4 : Fixing issues in parallel merge

--- so4/so_1.cc
+++ so4/so_1.cc
@@ -8,19 +8,15 @@
 
 typedef unsigned long long data_t;
 
-void psort(unsigned int n, data_t *data) {
-    // FIXME: Implement a more efficient parallel sorting algorithm for the CPU,
-    // using the basic idea of merge sort.
-    std::sort(data, data + n);
-}
 
-void my_merge(unsigned int start,unsigned int mid, unsigned int end, data_t *data) {
-  data_t *temp = (data_t *)malloc((end - start + 1) * sizeof(data_t));
-  unsigned int i1 = start;
-  unsigned int i2 = mid;
+void my_merge_serial(int start,int mid, int end, data_t *data) {
+
+  data_t *temp = (data_t *)malloc((end - start) * sizeof(data_t));
+  int i1 = start;
+  int i2 = mid;
   unsigned t = 0;
 
-  while(i1 <= mid-1 && i2 <=end ) {
+  while(i1 < mid && i2 < end ) {
     if (data[i1] < data[i2]) {
       temp[t] = data[i1];
       i1++;
@@ -31,39 +27,59 @@ void my_merge(unsigned int start,unsigned int mid, unsigned int end, data_t *dat
     t++;
   }
 
-  while (i1 <= mid-1) {
+  while (i1 < mid) {
     temp[t] = data[i1];
     i1++;
     t++;
   }
 
-  while (i2 <= end) {
+  while (i2 < end) {
     temp[t] = data[i2];
     i2++;
     t++;
   }
 
   // Final copy back to the main memory
-  std::memcpy(data + start, temp, (end-start+1) * sizeof(data_t));
+  std::memcpy(data + start, temp, (end-start) * sizeof(data_t));
 
   free(temp);
+}
+
+int custom_bs(int start, int end, data_t value, data_t *data) {
+  if (end == start) {
+    return end;
+  }
+  unsigned mid = (end + start) / 2;
+  if (data[mid] == value) {
+    return mid;
+  } else if (data[mid] < value) {
+    return custom_bs(mid + 1, end, value, data);
+  } else {
+    return custom_bs(start, mid, value, data);
+  }
 }
 
 // s1 <= e1 <= s2 <= e2                     (All indices on data)
 // scratch_start <= scratch_end             (All indices on scratch)
 void my_merge_parallel(
-                       unsigned int s1,
-                       unsigned int e1,
-                       unsigned int s2,
-                       unsigned int e2,
+                       int s1,
+                       int e1,
+                       int s2,
+                       int e2,
                        data_t *data,
-                       unsigned int scratch_start,
-                       unsigned int scratch_end,
+                       int scratch_start,
+                       int scratch_end,
                        data_t *scratch
                        ) {
 
-  unsigned d1 = e1 - s1;
-  unsigned d2 = e2 - s2;
+  std::cout << "MERGE " << s1 << " " << e1 << " " << s2 << " " << e2 << " " << scratch_start << " " << scratch_end << "\n";
+  int d1 = e1 - s1;
+  int d2 = e2 - s2;
+  std::cout << "BASE CASE " << d1 << " " << d2 << "\n";
+
+  if (d1 <= 0 || d2 <= 0) {
+    return;
+  }
 
   unsigned s3, e3;           // Index bounds for the larger list from the two.
   unsigned s4, e4;           // Index bounds for the smaller list from the two.
@@ -92,68 +108,53 @@ void my_merge_parallel(
   // mid point of scratch would be first half elements of largest array
   // + first half element of smaller array)
 
-  unsigned mid_scratch = (large_mid - s3) + (found - s4) + 1;
+  unsigned mid_scratch = scratch_start + (large_mid - s3) + (found - s4);
 
   // Set the min value of the scratch buffer
   scratch[mid_scratch] = m_value;
 
-  // First Half
-  unsigned int half1_1 = s3;
-  unsigned int half1_2 = large_mid;
-
-  // Second Half
-  unsigned int half2_1 = ;
-  unsigned int half2_2 = end;
-
   // Setup the recursion
-
   my_merge_parallel(s3, large_mid, s4, found + 1, data, scratch_start, mid_scratch, scratch);
   my_merge_parallel(large_mid, e3, found + 1, e4, data, mid_scratch + 1, scratch_end, scratch);
 
   // Final copy back to the main memory
-  std::memcpy(data + s3, scratch, (mid_scratch - scratch_start) * sizeof(data_t));
-  std::memcpy(data + large_mid, scratch, (scratch_end - mid_scratch + 1) * sizeof(data_t));
+  std::memcpy(data + s1, scratch + scratch_start, (scratch_end-scratch_start) * sizeof(data_t));
+
 
 }
 
-void my_sort_partial(unsigned int low, unsigned int high, data_t *data) {
-  std::cout << "PARTIAL SORT " << low << " " << high << "\n";
-  if (low == high) {
+// data[low, high)
+void my_sort_partial(int low, int high, data_t *data, data_t *scratch) {
+  std::cout << "SORT " << low << " " << high << "\n";
+  // If data range contains only 1 element.
+  if (high - low == 1) {
     return;
   }
-  if (high - low == 1) {
-    if (data[high] < data[low]) {
-      data[high] = data[low] ^ data[high];
-      data[low] = data[high] ^ data[low];
-      data[high] = data[low] ^ data[high];
+  // If data range contains only 2 elements.
+  if (high - low == 2) {
+    if (data[high-1] < data[low]) {
+      data[high-1]   = data[low]    ^ data[high-1];
+      data[low]      = data[high-1] ^ data[low];
+      data[high-1]   = data[low]    ^ data[high-1];
     }
     return;
   }
-  unsigned mid = (high + low + 1) / 2;
+  unsigned mid = (high + low) / 2;
 
-  #pragma omp task
-  my_sort_partial(low, mid-1, data);
+  my_sort_partial(low, mid, data, scratch);
+  my_sort_partial(mid, high, data, scratch);
 
-  #pragma omp task
-  my_sort_partial(mid, high, data);
-
-  #pragma omp taskwait
-  my_merge(low, mid, high, data);
+  my_merge_parallel(low, mid, mid, high, data, low, high, scratch);
 
 }
 
-void my_sort(unsigned int n, data_t *data) {
-  #pragma omp parallel
-  #pragma omp single
-  {
-    my_sort_partial(0, n-1, data);
-  }
-  #pragma omp taskwait
-}
 
-void print_l(unsigned int n, data_t *data) {
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+void print_l(int n, data_t *data) {
   std::cout << "\n";
-  for (unsigned int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++) {
     std::cout << data[i] << " ";
   }
   std::cout << "\n";
@@ -161,8 +162,9 @@ void print_l(unsigned int n, data_t *data) {
 
 int main() {
 
-  const unsigned int count = 8;
-  data_t *data = (data_t *)malloc(count * sizeof(data));
+  const int count = 3;
+  data_t *data = (data_t *)malloc(count * sizeof(data_t));
+  data_t *scratch = (data_t *)malloc(count * sizeof(data_t));
 
   std::random_device seed;
   std::mt19937 gen{seed()}; // seed the generator
@@ -171,13 +173,19 @@ int main() {
     data[i] = (data_t)dist(gen);
   }
   unsigned long before, after;
+  data[0] = 3;
+  data[1] = 5;
+  data[2] = 1;
+  //data[3] = 4;
   print_l(count, data);
 
   before = __rdtsc();
-  my_sort(count, data);
+  my_sort_partial(0, count, data, scratch);
   after = __rdtsc();
 
   print_l(count, data);
+
+  print_l(count, scratch);
 
   std::cout << "Cycles - " << after - before;
   free(data);
